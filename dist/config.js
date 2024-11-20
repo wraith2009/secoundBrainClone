659b5188c377f665ddf007a4ee25fcf02efe6663
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-11-21 00:23:29 +0530

    Add initial server setup with Express, MongoDB connection, and error handling

---
+++ dist/config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MONGO_URI = exports.PORT = void 0;
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+exports.PORT = process.env.PORT || 3000;
+exports.MONGO_URI = process.env.MONGO_URI ||
+    "mongodb+srv://rbh97995:1sHGU5Wd9szu9LdO@cluster0.3y6ur.mongodb.net/";
