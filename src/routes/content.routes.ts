38c99d9707b09c43a0749875bb2b7a99e33d26c7
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-11-21 12:14:44 +0530

    Remove authentication middleware from GET content route

--- src/routes/content.routes.ts
+++ src/routes/content.routes.ts
@@ -10,7 +10,7 @@ import { jwtAuth } from "../middleware/auth.middleware";
 const ContentRouter = Router();
 
 ContentRouter.route("/create-content").post(jwtAuth, CreateContent);
-ContentRouter.route("/get-content").post(jwtAuth, GetContent);
+ContentRouter.route("/get-content").post(GetContent);
 ContentRouter.route("/delete-content").post(jwtAuth, DeleteContent);
 ContentRouter.route("/update-content").post(jwtAuth, UpdateContent);
 
